531fbeb419f9edb8b686018e03e7154673d56ce2
José Rodrigo <kaomidev@gmail.com>
2020-02-08 20:44:33 -0300

    Adding tests

--- src/test/kotlin/tech/kaomidev/springsecurityjwt/SpringSecurityJwtApplicationTests.kt
+++ src/test/kotlin/tech/kaomidev/springsecurityjwt/SpringSecurityJwtApplicationTests.kt
@@ -1,13 +1,53 @@
 package tech.kaomidev.springsecurityjwt
 
+import com.fasterxml.jackson.core.type.TypeReference
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import tech.kaomidev.springsecurityjwt.web.dto.AuthResponseDto
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringSecurityJwtApplicationTests {
 
+	@Autowired
+	private lateinit var mvc: MockMvc
+
+	@Autowired
+	private lateinit var jacksonConverter: MappingJackson2HttpMessageConverter
+
 	@Test
-	fun contextLoads() {
+	fun `Should not allow access to unauthenticated users`() {
+		mvc.perform(MockMvcRequestBuilders.get("/users/me")).andExpect(status().isForbidden)
 	}
 
+	@Test
+	fun `Should authenticate and allow user access`() {
+		val username = "user"
+		val password = "123"
+
+		val body = """{"username":"$username","password":"$password"}"""
+
+		val result = mvc.perform(MockMvcRequestBuilders.post("/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(body))
+				.andExpect(status().isOk)
+				.andReturn()
+
+		val resultContent: String = result.response.contentAsString
+
+		var response: AuthResponseDto = jacksonConverter.objectMapper
+				.readValue(resultContent, AuthResponseDto::class.java)
+
+		mvc.perform(MockMvcRequestBuilders.get("/users/me")
+				.header("Authorization", "Bearer ${response.token}"))
+				.andExpect(status().isOk)
+	}
 }
